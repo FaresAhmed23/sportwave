477ecc144f301df057e65d17c0cfd168387ecda3
Fares Ahmed Mohamed Abd El Salam <fero32517@gmail.com>
2025-10-07 13:08:13 +0300

    123

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Product, Order, Customer } from './types';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://store-back-gold.vercel.app/api';
 
 const api = axios.create({
   baseURL: API_URL,
